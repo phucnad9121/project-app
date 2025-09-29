0ef776f7f0869138ffd935e080961a687f3eaf7a
thanhxuan2k5 <xuanng205@gmail.com>
2025-09-29 17:24:23 +0700

    Xuan day lan 3

--- app/src/main/java/com/example/project_btl/SplashActivity.java
+++ app/src/main/java/com/example/project_btl/SplashActivity.java
@@ -1,24 +1,24 @@
 package com.example.project_btl;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_splash);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        View btnSignIn = findViewById(R.id.btnSignIn);
+        if (btnSignIn != null) {
+            btnSignIn.setOnClickListener(v -> {
+                Intent intent = new Intent(SplashActivity.this, SignInactivity.class);
+                startActivity(intent);
+            });
+        }
+    }
 }
